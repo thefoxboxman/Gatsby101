7b1afe0cd0ee2c4dc198c2d48f5a9447d459385f
F John De Costa <john@mayerfleur.com>
2020-03-08 10:36:02 +1000

    Setting up pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <div>
+	<h1>Home Page</h1>
+	Hello world!
+
+	</div>
---
+++ src/pages/product.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function h1() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <h2>This is the product listing page</h2>
+    </div>
+  )
+}
